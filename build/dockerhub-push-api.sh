3586462622926b4c661d0cc6e22ca78168c1eb68
Munsy <tmunson@wsu.edu>
2020-01-07 20:34:36 -0800

    update build

--- build/dockerhub-push-api.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp ./build/api/Dockerfile ./Dockerfile
-docker build -t munsy/art-api .
-rm ./Dockerfile
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push munsy/art-api
--- build/dockerhub-push-ui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp ./build/ui/Dockerfile ./Dockerfile
-docker build -t munsy/art-ui .
-rm ./Dockerfile
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push munsy/art-ui
