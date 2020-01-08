f5490deebcde9a8bd6d240e03c8c9b2379b71a00
Munsy <tmunson@wsu.edu>
2020-01-07 21:09:52 -0800

    update build script

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t munsy/art:latest
+docker build -t munsy/art:latest .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push munsy/art:latest
