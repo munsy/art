49d646f52156b5c8c2fcfb83554fa8708b4080b8
Munsy <tmunson@wsu.edu>
2020-01-09 20:39:27 -0800

    fix cli bug, display all of 2nd most downloaded files

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: () => { 
+  apiURL: (): string => { 
   	var loc = window.location.href;
   	return loc.endsWith('/') ? loc.slice(0, -1) : loc;
   }
